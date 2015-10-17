e1c3da040f74f8721c3fa751374790c1119ff5a7
bob555com <bob555com@sina.com>
2015-10-17 16:11:31 +0800

    Create topad.js

---
+++ js/topad.js
@@ -0,0 +1,8 @@
+//顶部广告
+document.write("<a href='http://www.pj8280.com/?intr=pjcaiwang' target='_blank'><img src='ggtu1/pj5575-960.gif' border='0'/></a>");
+document.write("<a href='http://bet99999.com/?f=bet368' target='_blank'><img src='ggtu1/550277-960.gif' border='0'/></a>");
+document.write("<a href='http://www.pj8280.com/?intr=pjcaiwang' target='_blank'><img src='ggtu1/19333-960.gif' border='0'/></a>");
+document.write("<a href='http://www.hg5669.com/?intr=1888' target='_blank'><img src='ggtu1/hg5669-960.gif' border='0'/></a>");
+document.write("<a href='' target='_blank'><img src='images/zz960X50.gif' border='0'/></a>");
+document.write("<a href='' target='_blank'><img src='images/zz960X50.gif' border='0'/></a>");
+document.write("<a href='' target='_blank'><img src='images/zz960X50.gif' border='0'/></a>");
