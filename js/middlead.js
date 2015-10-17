439c3f7f830952d5e4990bbf3e64d0d47dfa7b6c
bob555com <bob555com@sina.com>
2015-10-17 16:12:40 +0800

    Create middlead.js

---
+++ js/middlead.js
@@ -0,0 +1,13 @@
+//中部广告
+document.write("<!---1行--->");
+document.write("<li class=\"zl\"> <a href=\"http://www.pj8280.com/?intr=pjcaiwang\" target=\"_blank\" class=\"pic\"><img src=\"ggtu2/pj5575-130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"http://www.pj8280.com/?intr=pjcaiwang\" target=\"_blank\" class=\"title\">已交纳500万押金</a> </li>");
+document.write("<li class=\"zl\"> <a href=\"http://www.hg5669.com/?intr=1888\" target=\"_blank\" class=\"pic\"><img src=\"ggtu2/550227-130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"http://www.hg5669.com/?intr=1888\" target=\"_blank\" class=\"title\">已交纳500万押金</a> </li>");
+document.write("<li class=\"zl\"> <a href=\"http://www.pj8280.com/?intr=pjcaiwang\" target=\"_blank\" class=\"pic\"><img src=\"ggtu2/19333-130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"http://www.pj8280.com/?intr=pjcaiwang\" target=\"_blank\" class=\"title\">已交纳500万押金</a> </li>");
+document.write("<li class=\"zl\"> <a href=\"http://www.hg5669.com/?intr=1888\" target=\"_blank\" class=\"pic\"><img src=\"ggtu2/hg5669-130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"http://bet99999.com/?f=bet368\" target=\"_blank\" class=\"title\">已交纳500万押金</a> </li>");
+document.write("<li class=\"zl\"> <a href=\"#\" target=\"_blank\" class=\"pic\"><img src=\"images/zz130x130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"\" target=\"_blank\" class=\"title\">已交纳200万押金</a> </li>");
+document.write("<!---2行--->");
+document.write("<li class=\"yh\"> <a href=\"#\" target=\"_blank\" class=\"pic\"><img src=\"images/zz130x130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"#\" target=\"_blank\" class=\"title\">已交纳200万押金</a> </li>");
+document.write("<li class=\"yh\"> <a href=\"#\" target=\"_blank\" class=\"pic\"><img src=\"images/zz130x130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"#\" target=\"_blank\" class=\"title\">已交纳200万押金</a> </li>");
+document.write("<li class=\"yh\"> <a href=\"#\" target=\"_blank\" class=\"pic\"><img src=\"images/zz130x130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"#\" target=\"_blank\" class=\"title\">已交纳200万押金</a> </li>");
+document.write("<li class=\"yh\"> <a href=\"#\" target=\"_blank\" class=\"pic\"><img src=\"images/zz130x130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"#\" target=\"_blank\" class=\"title\">已交纳200万押金</a> </li>");
+document.write("<li class=\"yh\"> <a href=\"#\" target=\"_blank\" class=\"pic\"><img src=\"images/zz130x130.gif\" width=\"129\" height=\"129\" alt=\"\" /></a><a href=\"#\" target=\"_blank\" class=\"title\">已交纳200万押金</a> </li>");
